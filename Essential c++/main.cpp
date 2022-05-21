9c6e260844ee49d8495210e342ae7419af85d57e
robinhan10211021 <1247762747@qq.com>
2022-05-21 06:34:01 +0000

    good

---
+++ Essential c++/main.cpp
@@ -0,0 +1,20 @@
+#include<vector>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+        
+        return ;
+    }
+
+int main(int argc, char const *argv[])
+{
+    vector<int> nums = {1,1,2};
+    vector<int> expectedNums = {1,2};
+    
+    int k = removeDuplicates(nums);
+    
+    return 0;
+}
+
+
+
